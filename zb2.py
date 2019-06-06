7db5e8bded5bb0747b4a1fb1bab4fcb47401a3d9
NANAmine <1486496165@qq.com>
2019-06-06 15:05:25 +0800

    指标1、2完成测试 git co -m ‘指标1、2完成测试

--- zb2.py
+++ zb2.py
@@ -2,16 +2,16 @@ import tools
 
 #指标2-主函数
 def main():
-    a_file = '战略发展部经营分析201904.xlsx' #数据源表
+    a_file = '战略发展部经营分析201903.xlsx' #数据源表
     a_by_name_pl = '品类' #数据源sheet
     a_by_name_sntq = '上年同期'
     b_file = 'dm_jsc_fact_kpi_target_pl.xls' #数据存储表
     b_file_sheet = 'dm_jsc_fact_kpi_target_pl'
 
-    #指标2-本部对外批发
-    #BNLJ
+    # #指标2-本部对外批发
+    # # #BNLJ
     a = 2   #获取数据的起始行数
-    b = 3448  #填报数据的起始行数
+    b = 3507  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,148]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -20,11 +20,17 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #TBZF
+    # # #TBZF
     a = 2   #获取数据的起始行数
-    b = 3448  #填报数据的起始行数
+    b = 3507  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,151]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -35,8 +41,52 @@ def main():
         for i in range(len(a_cell_pl_list)):
             if a_cell_pl_list[i] == "":
                 a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
             print(amount)
+        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    #SNTQZ                                                                                                       注意核对公式
+    a = 164   #获取数据的起始行数
+    b = 3507  #填报数据的起始行数
+    for x in range(0,8):
+        list = [[a+x,157]] #上年同期值
+        b_row = b+x   #数据坐标行数
+        print(b_row)
+        b_column = 22   #数据坐标列数
+        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
+        amount = 0    #坐标值求和
+        for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
+            amount = a_cell_pl_list[i] + amount
+            print(amount)
+        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    #BNLJ2
+    a = 2   #获取数据的起始行数
+    c = 3206   #增量合并本年累计营收填报数据的起始行数
+    d = 3507   #存量本年累计营收填报数据的起始行数
+    for x in range(0,8):
+        list2 = [[c+x,7]] #增量合并本年累计营收
+        b_row = d+x   #数据坐标行数
+        print(b_row)
+        b_column = 24   #数据坐标列数
+        a_cell_pl_list = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
+        amount = 0
+        for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
+            amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
 
     #指标2-传统重点门店                                                                                              计算公式置后
@@ -46,7 +96,7 @@ def main():
     #指标2-柬中免
     #BNLJ
     a = 2   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
+    b = 3499  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,99]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -55,11 +105,17 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #
     #TBZF
     a = 2   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
+    b = 3499  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,102]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -68,11 +124,17 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
     #SNTQZ                                                                                                       注意核对公式
     a = 164   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
+    b = 3499  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,148]] #上年同期值
         b_row = b+x   #数据坐标行数
@@ -81,6 +143,12 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
             print(amount)
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
@@ -89,9 +157,9 @@ def main():
 
     #BNLJ2
     a = 2   #获取数据的起始行数
-    b = 3471   #合并本年累计营收填报数据的起始行数
-    c = 3374   #增量合并本年累计营收填报数据的起始行数
-    d = 3406   #存量本年累计营收填报数据的起始行数
+    b = 3411   #合并本年累计营收填报数据的起始行数
+    c = 3459   #增量合并本年累计营收填报数据的起始行数
+    d = 3499   #存量本年累计营收填报数据的起始行数
     for x in range(0,8):
         list1 = [[b+x,7]] #合并本年累计营收
         list2 = [[c+x,7]] #增量合并本年累计营收
@@ -102,13 +170,25 @@ def main():
         a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
         amount = 0
         for i in range(len(a_cell_pl_list1)):
+            if a_cell_pl_list1[i] == "":
+                a_cell_pl_list1[i] = 0
+            elif a_cell_pl_list1[i] == -1e-06:
+                a_cell_pl_list1[i] == 0
+            elif a_cell_pl_list1[i] == 1e-06:
+                a_cell_pl_list1[i] = 0
+            if a_cell_pl_list2[i] == "":
+                a_cell_pl_list2[i] = 0
+            elif a_cell_pl_list2[i] == -1e-06:
+                a_cell_pl_list2[i] == 0
+            elif a_cell_pl_list2[i] == 1e-06:
+                a_cell_pl_list2[i] = 0
             amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
 
     #指标2-三亚店
     #BNLJ
     a = 2   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
+    b = 3475  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,36]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -117,11 +197,17 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)
     #TBZF
     a = 2   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
+    b = 3475  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,39]] #本部对外批发
         b_row = b+x   #数据坐标行数
@@ -132,11 +218,15 @@ def main():
         for i in range(len(a_cell_pl_list)):
             if a_cell_pl_list[i] == "":
                 a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
     #SNTQZ                                                                                                       注意核对公式
     a = 164   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
+    b = 3475  #填报数据的起始行数
     for x in range(0,8):
         list = [[a+x,139]] #上年同期值
         b_row = b+x   #数据坐标行数
@@ -145,6 +235,12 @@ def main():
         a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
         amount = 0    #坐标值求和
         for i in range(len(a_cell_pl_list)):
+            if a_cell_pl_list[i] == "":
+                a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
             print(amount)
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
@@ -153,9 +249,9 @@ def main():
 
     #BNLJ2
     a = 2   #获取数据的起始行数
-    b = 3471   #合并本年累计营收填报数据的起始行数
-    c = 3374   #增量合并本年累计营收填报数据的起始行数
-    d = 3510   #存量本年累计营收填报数据的起始行数
+    b = 3411   #合并本年累计营收填报数据的起始行数
+    c = 3459   #增量合并本年累计营收填报数据的起始行数
+    d = 3475   #存量本年累计营收填报数据的起始行数
     for x in range(0,8):
         list1 = [[b+x,7]] #合并本年累计营收
         list2 = [[c+x,7]] #增量合并本年累计营收
@@ -166,8 +262,17 @@ def main():
         a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
         amount = 0
         for i in range(len(a_cell_pl_list1)):
+            if a_cell_pl_list1[i] == "":
+                a_cell_pl_list1[i] = 0
+            elif a_cell_pl_list1[i] == -1e-06:
+                a_cell_pl_list1[i] == 0
+            elif a_cell_pl_list1[i] == 1e-06:
+                a_cell_pl_list1[i] = 0
+            if a_cell_pl_list2[i] == "":
+                a_cell_pl_list2[i] = 0
+            elif a_cell_pl_list2[i] == -1e-06:
+                a_cell_pl_list2[i] == 0
+            elif a_cell_pl_list2[i] == 1e-06:
+                a_cell_pl_list2[i] = 0
             amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-
-if __name__=="__main__":
-    main2()
--- zb2a.py
+++ zb2a.py
@@ -8,166 +8,213 @@ def main():
     b_file = 'dm_jsc_fact_kpi_target_pl.xls' #数据存储表
     b_file_sheet = 'dm_jsc_fact_kpi_target_pl'
 
-    #指标2-本部对外批发
-    #BNLJ
+    # #指标2-本部对外批发
+    # # #BNLJ
+    # a = 2   #获取数据的起始行数
+    # b = 3507  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,148]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 7   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         if a_cell_pl_list[i] == "":
+    #             a_cell_pl_list[i] = 0
+    #         elif a_cell_pl_list[i] == -1e-06:
+    #             a_cell_pl_list[i] == 0
+    #         elif a_cell_pl_list[i] == 1e-06:
+    #             a_cell_pl_list[i] = 0
+    #         amount = a_cell_pl_list[i] + amount
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # # # #TBZF
+    # a = 2   #获取数据的起始行数
+    # b = 3507  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,151]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 8   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         if a_cell_pl_list[i] == "":
+    #             a_cell_pl_list[i] = 0
+    #         elif a_cell_pl_list[i] == -1e-06:
+    #             a_cell_pl_list[i] == 0
+    #         elif a_cell_pl_list[i] == 1e-06:
+    #             a_cell_pl_list[i] = 0
+    #         amount = a_cell_pl_list[i] + amount
+    #         print(amount)
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #SNTQZ                                                                                                       注意核对公式
+    # a = 164   #获取数据的起始行数
+    # b = 3507  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,157]] #上年同期值
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 22   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         if a_cell_pl_list[i] == "":
+    #             a_cell_pl_list[i] = 0
+    #         elif a_cell_pl_list[i] == -1e-06:
+    #             a_cell_pl_list[i] == 0
+    #         elif a_cell_pl_list[i] == 1e-06:
+    #             a_cell_pl_list[i] = 0
+    #         amount = a_cell_pl_list[i] + amount
+    #         print(amount)
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    #BNLJ2
     a = 2   #获取数据的起始行数
-    b = 3448  #填报数据的起始行数
+    c = 3206   #增量合并本年累计营收填报数据的起始行数
+    d = 3507   #存量本年累计营收填报数据的起始行数
     for x in range(0,8):
-        list = [[a+x,148]] #本部对外批发
-        b_row = b+x   #数据坐标行数
+        list2 = [[c+x,7]] #增量合并本年累计营收
+        b_row = d+x   #数据坐标行数
         print(b_row)
-        b_column = 7   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #TBZF
-    a = 2   #获取数据的起始行数
-    b = 3448  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,151]] #本部对外批发
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 8   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
+        b_column = 24   #数据坐标列数
+        a_cell_pl_list = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
+        amount = 0
         for i in range(len(a_cell_pl_list)):
             if a_cell_pl_list[i] == "":
                 a_cell_pl_list[i] = 0
+            elif a_cell_pl_list[i] == -1e-06:
+                a_cell_pl_list[i] == 0
+            elif a_cell_pl_list[i] == 1e-06:
+                a_cell_pl_list[i] = 0
             amount = a_cell_pl_list[i] + amount
-            print(amount)
         tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
 
     #指标2-传统重点门店                                                                                              计算公式置后
     #
 
 
-    #指标2-柬中免
-    #BNLJ
-    a = 2   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,99]] #本部对外批发
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 7   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #
-    #TBZF
-    a = 2   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,102]] #本部对外批发
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 8   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #SNTQZ                                                                                                       注意核对公式
-    a = 164   #获取数据的起始行数
-    b = 3406  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,148]] #上年同期值
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 22   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-            print(amount)
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #TBZF2                                                                                           取指标1存量同比增幅，香化食品置后
+    # #指标2-柬中免
+    # #BNLJ
+    # a = 2   #获取数据的起始行数
+    # b = 3406  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,99]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 7   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         amount = a_cell_pl_list[i] + amount
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #
+    # #TBZF
+    # a = 2   #获取数据的起始行数
+    # b = 3406  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,102]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 8   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         amount = a_cell_pl_list[i] + amount
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #SNTQZ                                                                                                       注意核对公式
+    # a = 164   #获取数据的起始行数
+    # b = 3406  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,148]] #上年同期值
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 22   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         amount = a_cell_pl_list[i] + amount
+    #         print(amount)
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #TBZF2                                                                                           取指标1存量同比增幅，香化食品置后
     
 
-    #BNLJ2
-    a = 2   #获取数据的起始行数
-    b = 3471   #合并本年累计营收填报数据的起始行数
-    c = 3374   #增量合并本年累计营收填报数据的起始行数
-    d = 3406   #存量本年累计营收填报数据的起始行数
-    for x in range(0,8):
-        list1 = [[b+x,7]] #合并本年累计营收
-        list2 = [[c+x,7]] #增量合并本年累计营收
-        b_row = d+x   #数据坐标行数
-        print(b_row)
-        b_column = 24   #数据坐标列数
-        a_cell_pl_list1 = tools.excel_cell_bylist(b_file,b_file_sheet,list1)   #获取合并本年累计营收
-        a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
-        amount = 0
-        for i in range(len(a_cell_pl_list1)):
-            amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #BNLJ2
+    # a = 2   #获取数据的起始行数
+    # b = 3471   #合并本年累计营收填报数据的起始行数
+    # c = 3374   #增量合并本年累计营收填报数据的起始行数
+    # d = 3406   #存量本年累计营收填报数据的起始行数
+    # for x in range(0,8):
+    #     list1 = [[b+x,7]] #合并本年累计营收
+    #     list2 = [[c+x,7]] #增量合并本年累计营收
+    #     b_row = d+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 24   #数据坐标列数
+    #     a_cell_pl_list1 = tools.excel_cell_bylist(b_file,b_file_sheet,list1)   #获取合并本年累计营收
+    #     a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
+    #     amount = 0
+    #     for i in range(len(a_cell_pl_list1)):
+    #         amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
 
-    #指标2-三亚店
-    #BNLJ
-    a = 2   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,36]] #本部对外批发
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 7   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)
-    #TBZF
-    a = 2   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,39]] #本部对外批发
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 8   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            if a_cell_pl_list[i] == "":
-                a_cell_pl_list[i] = 0
-            amount = a_cell_pl_list[i] + amount
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #SNTQZ                                                                                                       注意核对公式
-    a = 164   #获取数据的起始行数
-    b = 3510  #填报数据的起始行数
-    for x in range(0,8):
-        list = [[a+x,139]] #上年同期值
-        b_row = b+x   #数据坐标行数
-        print(b_row)
-        b_column = 22   #数据坐标列数
-        a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
-        amount = 0    #坐标值求和
-        for i in range(len(a_cell_pl_list)):
-            amount = a_cell_pl_list[i] + amount
-            print(amount)
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-    #TBZF2                                                                                           取指标1存量同比增幅，香化食品置后
+    # #指标2-三亚店
+    # #BNLJ
+    # a = 2   #获取数据的起始行数
+    # b = 3510  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,36]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 7   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         amount = a_cell_pl_list[i] + amount
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)
+    # #TBZF
+    # a = 2   #获取数据的起始行数
+    # b = 3510  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,39]] #本部对外批发
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 8   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_pl,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         if a_cell_pl_list[i] == "":
+    #             a_cell_pl_list[i] = 0
+    #         amount = a_cell_pl_list[i] + amount
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #SNTQZ                                                                                                       注意核对公式
+    # a = 164   #获取数据的起始行数
+    # b = 3510  #填报数据的起始行数
+    # for x in range(0,8):
+    #     list = [[a+x,139]] #上年同期值
+    #     b_row = b+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 22   #数据坐标列数
+    #     a_cell_pl_list = tools.excel_cell_bylist(a_file,a_by_name_sntq,list)   #获取多组坐标值
+    #     amount = 0    #坐标值求和
+    #     for i in range(len(a_cell_pl_list)):
+    #         amount = a_cell_pl_list[i] + amount
+    #         print(amount)
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
+    # #TBZF2                                                                                           取指标1存量同比增幅，香化食品置后
     
 
-    #BNLJ2
-    a = 2   #获取数据的起始行数
-    b = 3471   #合并本年累计营收填报数据的起始行数
-    c = 3374   #增量合并本年累计营收填报数据的起始行数
-    d = 3510   #存量本年累计营收填报数据的起始行数
-    for x in range(0,8):
-        list1 = [[b+x,7]] #合并本年累计营收
-        list2 = [[c+x,7]] #增量合并本年累计营收
-        b_row = d+x   #数据坐标行数
-        print(b_row)
-        b_column = 24   #数据坐标列数
-        a_cell_pl_list1 = tools.excel_cell_bylist(b_file,b_file_sheet,list1)   #获取合并本年累计营收
-        a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
-        amount = 0
-        for i in range(len(a_cell_pl_list1)):
-            amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
-        tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
-
-if __name__=="__main__":
-    main2()
+    # #BNLJ2
+    # a = 2   #获取数据的起始行数
+    # b = 3471   #合并本年累计营收填报数据的起始行数
+    # c = 3374   #增量合并本年累计营收填报数据的起始行数
+    # d = 3510   #存量本年累计营收填报数据的起始行数
+    # for x in range(0,8):
+    #     list1 = [[b+x,7]] #合并本年累计营收
+    #     list2 = [[c+x,7]] #增量合并本年累计营收
+    #     b_row = d+x   #数据坐标行数
+    #     print(b_row)
+    #     b_column = 24   #数据坐标列数
+    #     a_cell_pl_list1 = tools.excel_cell_bylist(b_file,b_file_sheet,list1)   #获取合并本年累计营收
+    #     a_cell_pl_list2 = tools.excel_cell_bylist(b_file,b_file_sheet,list2)   #获取增量合并本年累计营收
+    #     amount = 0
+    #     for i in range(len(a_cell_pl_list1)):
+    #         amount = a_cell_pl_list1[i] - a_cell_pl_list2[i]
+    #     tools.testXlwt_cell(b_file, b_file_sheet, b_row, b_column, amount)   #数据写入报表
